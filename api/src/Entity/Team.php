d88e9d7a83742ef7961c8a61c8fb88b624941feb
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-02-17 19:39:59 +0100

    :sparkles: Delete Team

--- api/src/Entity/Team.php
+++ api/src/Entity/Team.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
@@ -78,8 +79,22 @@ use Symfony\Component\Validator\Constraints as Assert;
                     'teams:item:read'
                 ]
             ],
-            security: 'is_granted("' . GlobalHelper::ROLE_ADMIN . '") or (is_granted("' . GlobalHelper::ROLE_USER . '") and user.isInTeam(object))'
+            security: '
+                is_granted("' . GlobalHelper::ROLE_ADMIN . '") or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and user.isInTeam(object)) or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and object.getManager() === user)
+            '
         ),
+        new Delete(
+            normalizationContext: [
+                'openapi_definition_name' => 'DeleteItem'
+            ],
+            security: '
+                is_granted("' . GlobalHelper::ROLE_ADMIN . '") or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and object.getManager() === user)
+            '
+        )
+
     ]
 )]
 class Team implements ManagerAwareInterface
