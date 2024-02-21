82fbd1a305205a633a57580e5d4e102eee3b8a5a
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-21 17:59:00 +0100

    :sparkles: Get single BusinessModelCanvas

--- api/src/Entity/BusinessModelCanvas.php
+++ api/src/Entity/BusinessModelCanvas.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
@@ -76,6 +77,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             ],
             security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
             processor: ProjectDocumentPostDataPersister::class
+        ),
+        new Get(
+            uriTemplate: '/business_model_canvases/{id}',
+            requirements: [
+                'id' => '^[a-z0-9]+(?:-[a-z0-9]+)*$'
+            ],
+            normalizationContext: [
+                'openapi_definition_name' => 'GetItem',
+                'groups' => [
+                    'business_model_canvas:read',
+                    'business_model_canvas:item:read'
+                ]
+            ],
+            security: '
+                is_granted("' . GlobalHelper::ROLE_ADMIN . '") or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and user.isInTeam(object.getProject().getTeam())) or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and object.getProject().getTeam().getManager() === user)
+            '
         ),
     ]
 )]
