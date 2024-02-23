96a72698f25f5f05d7bd70c6510318accf503c96
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-23 17:21:29 +0100

    :sparkles: Get SMARTs

--- api/src/Entity/SMART.php
+++ api/src/Entity/SMART.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
 use App\Helper\GlobalHelper;
 use App\Model\TracingAwareInterface;
 use App\Model\Traits\TracingAwareTrait;
 use App\Repository\SMARTRepository;
 use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
+use App\StateProviders\ProjectDocumentByProjectCollectionDataProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -64,6 +67,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             processor: ProjectDocumentPostDataPersister::class
         ),
     ]
+)]
+#[ApiResource(
+    uriTemplate: '/projects/{id}/smarts',
+    shortName: 'smart',
+    operations: [new GetCollection()],
+    uriVariables: [
+        'id' => new Link(
+            toProperty: 'project',
+            fromClass: Project::class
+        )
+    ],
+    normalizationContext: [
+        'groups' => [
+            'smart:read'
+        ]
+    ],
+    security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+    provider: ProjectDocumentByProjectCollectionDataProvider::class
 )]
 class SMART implements TracingAwareInterface
 {
