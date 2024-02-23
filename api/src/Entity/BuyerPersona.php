8e7e49ab0317be441c98dbe9fd3bedd2c4b0bc72
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-23 17:38:49 +0100

    :sparkles: Post BuyerPersona

--- api/src/Entity/BuyerPersona.php
+++ api/src/Entity/BuyerPersona.php
@@ -2,7 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
+use App\Helper\GlobalHelper;
 use App\Repository\BuyerPersonaRepository;
+use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -14,6 +18,53 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  */
 #[ORM\Entity(repositoryClass: BuyerPersonaRepository::class)]
+#[ApiResource(
+    operations: [
+        new Post(
+            uriTemplate: '/buyer_personas',
+            openapiContext: [
+                'requestBody' => [
+                    'content' => [
+                        'application/ld+json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'project' => [
+                                        'type' => 'string',
+                                        'example' =>'/projects/{id}'
+                                    ],
+                                    'personalInfo' => [
+                                        'type' => 'string'
+                                    ],
+                                    'professionalInfo' => [
+                                        'type' => 'string'
+                                    ],
+                                    'goalsChallenges' => [
+                                        'type' => 'string'
+                                    ],
+                                    'communicationChannels' => [
+                                        'type' => 'string'
+                                    ],
+                                    'valuesFears' => [
+                                        'type' => 'string'
+                                    ],
+                                    'negativeInfo' => [
+                                        'type' => 'string'
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            normalizationContext: [
+                'openapi_definition_name' => 'PostCollection'
+            ],
+            security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+            processor: ProjectDocumentPostDataPersister::class
+        ),
+    ]
+)]
 class BuyerPersona
 {
     /**
