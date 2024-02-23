6607f8cd1c62f1db6cce2e39e5194c9a66ea4f45
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-23 17:40:22 +0100

    :sparkles: Get BuyerPersona collection

--- api/src/Entity/BuyerPersona.php
+++ api/src/Entity/BuyerPersona.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
 use App\Helper\GlobalHelper;
 use App\Repository\BuyerPersonaRepository;
 use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
+use App\StateProviders\ProjectDocumentByProjectCollectionDataProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -64,6 +67,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             processor: ProjectDocumentPostDataPersister::class
         ),
     ]
+)]
+#[ApiResource(
+    uriTemplate: '/projects/{id}/buyer_personas',
+    shortName: 'buyer_persona',
+    operations: [new GetCollection()],
+    uriVariables: [
+        'id' => new Link(
+            toProperty: 'project',
+            fromClass: Project::class
+        )
+    ],
+    normalizationContext: [
+        'groups' => [
+            'buyer_persona:read'
+        ]
+    ],
+    security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+    provider: ProjectDocumentByProjectCollectionDataProvider::class
 )]
 class BuyerPersona
 {
