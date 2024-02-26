e859037e6e5476f10e7166f943b9e015c4748136
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-02-26 09:54:18 +0100

    :sparkles: Get STP collection

--- api/src/Entity/STP.php
+++ api/src/Entity/STP.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
 use App\Helper\GlobalHelper;
 use App\Model\TracingAwareInterface;
 use App\Model\Traits\TracingAwareTrait;
 use App\Repository\STPRepository;
 use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
+use App\StateProviders\ProjectDocumentByProjectCollectionDataProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -58,6 +61,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             processor: ProjectDocumentPostDataPersister::class
         ),
     ]
+)]
+#[ApiResource(
+    uriTemplate: '/projects/{id}/stps',
+    shortName: 'stp',
+    operations: [new GetCollection()],
+    uriVariables: [
+        'id' => new Link(
+            toProperty: 'project',
+            fromClass: Project::class
+        )
+    ],
+    normalizationContext: [
+        'groups' => [
+            'stp:read'
+        ]
+    ],
+    security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+    provider: ProjectDocumentByProjectCollectionDataProvider::class
 )]
 class STP implements TracingAwareInterface
 {
