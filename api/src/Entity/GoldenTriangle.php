88f0286c80ce73ba81907dadb33d2c06c5d32e0e
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-02-26 11:14:27 +0100

    :sparkles: Get single GoldenTriangle

--- api/src/Entity/GoldenTriangle.php
+++ api/src/Entity/GoldenTriangle.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
@@ -81,6 +82,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             ],
             security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
             processor: ProjectDocumentPostDataPersister::class
+        ),
+        new Get(
+            uriTemplate: '/golden_triangles/{id}',
+            requirements: [
+                'id' => '^[a-z0-9]+(?:-[a-z0-9]+)*$'
+            ],
+            normalizationContext: [
+                'openapi_definition_name' => 'GetItem',
+                'groups' => [
+                    'golden_triangle:read',
+                    'golden_triangle:item:read'
+                ]
+            ],
+            security: '
+                is_granted("' . GlobalHelper::ROLE_ADMIN . '") or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and user.isInTeam(object.getProject().getTeam())) or
+                (is_granted("' . GlobalHelper::ROLE_USER . '") and object.getProject().getTeam().getManager() === user)
+            '
         ),
     ]
 )]
