84ae35a8ed3c8f76971f3ec3ab4729e437138b9c
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-02-26 11:13:30 +0100

    :sparkles: Get GoldenTriangle collection

--- api/src/Entity/GoldenTriangle.php
+++ api/src/Entity/GoldenTriangle.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
 use App\Helper\GlobalHelper;
 use App\Model\TracingAwareInterface;
 use App\Model\Traits\TracingAwareTrait;
 use App\Repository\GoldenTriangleRepository;
 use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
+use App\StateProviders\ProjectDocumentByProjectCollectionDataProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -80,6 +83,23 @@ use Symfony\Component\Validator\Constraints as Assert;
             processor: ProjectDocumentPostDataPersister::class
         ),
     ]
+)]
+#[ApiResource(
+    uriTemplate: '/projects/{id}/golden_triangles',
+    operations: [new GetCollection()],
+    uriVariables: [
+        'id' => new Link(
+            toProperty: 'project',
+            fromClass: Project::class
+        )
+    ],
+    normalizationContext: [
+        'groups' => [
+            'golden_triangle:read'
+        ]
+    ],
+    security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+    provider: ProjectDocumentByProjectCollectionDataProvider::class
 )]
 class GoldenTriangle implements TracingAwareInterface
 {
