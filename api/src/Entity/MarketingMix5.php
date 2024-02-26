13e6d8ab0d0660494791d36d481b7113b3a590b5
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-02-26 09:38:21 +0100

    :sparkles: Get MarketingMix5 collection

--- api/src/Entity/MarketingMix5.php
+++ api/src/Entity/MarketingMix5.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
 use App\Helper\GlobalHelper;
 use App\Model\TracingAwareInterface;
 use App\Model\Traits\TracingAwareTrait;
 use App\Repository\MarketingMix5Repository;
 use App\StateProcessor\Project\ProjectDocumentPostDataPersister;
+use App\StateProviders\ProjectDocumentByProjectCollectionDataProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -64,6 +67,24 @@ use Symfony\Component\Validator\Constraints as Assert;
             processor: ProjectDocumentPostDataPersister::class
         ),
     ]
+)]
+#[ApiResource(
+    uriTemplate: '/projects/{id}/marketing_mix_5s',
+    shortName: 'marketing_mix_5',
+    operations: [new GetCollection()],
+    uriVariables: [
+        'id' => new Link(
+            toProperty: 'project',
+            fromClass: Project::class
+        )
+    ],
+    normalizationContext: [
+        'groups' => [
+            'marketing_mix_5:read'
+        ]
+    ],
+    security: 'is_granted("' . GlobalHelper::ROLE_USER . '")',
+    provider: ProjectDocumentByProjectCollectionDataProvider::class
 )]
 class MarketingMix5 implements TracingAwareInterface
 {
