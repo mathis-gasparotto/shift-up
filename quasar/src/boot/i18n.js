5be4f589a87948c43da4a1732baf7fc6f76d950d
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-08-05 18:45:42 +0200

    feat(front): prepare for mutli-langue

--- quasar/src/boot/i18n.js
+++ quasar/src/boot/i18n.js
@@ -3,6 +3,9 @@ import messages from 'src/i18n'
 import { boot } from 'quasar/wrappers'
 import { Lang } from 'quasar'
 
+// Lang.set(Lang.getLocale())
+Lang.set('fr-FR')
+
 // Create I18n instance
 export const i18n = createI18n({
   locale: Lang.props.isoName || 'en-US',
